8a5b3e199d12f18440094555c0ce65eaf46c49a8
JoaoPaulo-creator <joaosarto@gmail.com>
2024-08-16 19:59:23 -0300

    feat: adicionado novo tema

--- init.lua
+++ init.lua
@@ -781,11 +781,16 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'onedark'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
     end,
+  },
+  {
+
+    'navarasu/onedark.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
   },
 
   -- Highlight todo, notes, etc in comments
