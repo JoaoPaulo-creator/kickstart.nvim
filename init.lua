cfc8c8c7bb707cb0bff99e556cd6e8a457a9bd73
JoaoPaulo-creator <joaosarto@gmail.com>
2024-11-12 23:05:32 -0300

    feat: adicionado plugin metals, para escrever em scala no neovim

--- init.lua
+++ init.lua
@@ -617,6 +617,32 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'scalameta/nvim-metals',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    ft = { 'scala', 'sbt', 'java' },
+    opts = function()
+      local metals_config = require('metals').bare_config()
+      metals_config.on_attach = function(client, bufnr)
+        -- your on_attach function
+      end
+
+      return metals_config
+    end,
+    config = function(self, metals_config)
+      local nvim_metals_group = vim.api.nvim_create_augroup('nvim-metals', { clear = true })
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = self.ft,
+        callback = function()
+          require('metals').initialize_or_attach(metals_config)
+        end,
+        group = nvim_metals_group,
+      })
+    end,
+  },
+
   { -- Autoformat
     'stevearc/conform.nvim',
     event = { 'BufWritePre' },
@@ -822,6 +848,11 @@ require('lazy').setup({
   {
     'navarasu/onedark.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
+  },
+  {
+    'shaunsingh/nord.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    lazy = false,
   },
   {
     'neanias/everforest-nvim',
